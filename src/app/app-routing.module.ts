98063ca4765e55046876851728bdd549373f5d22
Fernando Santillan <ferdiego88@gmail.com>
2022-08-02 10:52:12 -0500

    template

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,10 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-
+    {
+      path: 'vacunados',
+      loadChildren: () => import('./template/template.module').then(m => m.TemplateModule)
+    }
 ];
 
 @NgModule({
--- src/app/template/template-routing.module.ts
+++ src/app/template/template-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { InicioComponent } from './inicio/inicio.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'',
+    children: [
+      {path: 'inicio', component: InicioComponent},
+      {path:'**', redirectTo: 'inicio'}
+    ]
+
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
